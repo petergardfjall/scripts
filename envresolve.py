0e86ec15430463246bcfda0454276ec5536e1dd1
petergardfjall <peter.gardfjall.work@gmail.com>
2020-02-09 10:03:09 +0100

    remove envresolve.py: envsubst is better

--- envresolve.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#
-# A script that reads from stdin and prints the input back on stdout with
-# all environment variables references resolved.
-#
-
-import os, sys
-print os.path.expandvars(sys.stdin.read())
