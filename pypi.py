fe166de4950ef0a9ab7b9a4cfc66c50d2d2a729f
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2020-08-23 12:50:58 +0200

    pypi.py: fix redirection handling, optionally specify version on show

--- pypi.py
+++ pypi.py
@@ -8,6 +8,7 @@ import sys
 from urllib.parse import urlparse
 
 LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 class Response:
     def __init__(self, http_response):
@@ -21,22 +22,30 @@ class Response:
         http_response.close()
 
 
-def get_package_metadata(package_index, package):
-    u = urlparse(f"https://{package_index}/pypi/{package}/json")
+def get(url):
+    u = urlparse(url)
     conn = http.client.HTTPSConnection(u.netloc)
     try:
         conn.request("GET", u.path)
         resp = conn.getresponse()
         if resp.status in [301, 302]:
-            LOG.debug("pypi.org redirected: %d (%s): %s", resp.status, resp.reason, resp.headers["Location"])
+            LOG.debug("%s redirected: %d (%s): %s", u.netloc, resp.status, resp.reason, resp.headers["Location"])
             return get(resp.headers["Location"])
         return Response(resp)
     finally:
         conn.close()
 
-def get_package_metadata_or_die(package_index, package):
+def get_package_metadata(package_index, package, version=None):
+    pkg_path = package
+    if version:
+        pkg_path += f"/{version}"
+
+    return get(f"https://{package_index}/pypi/{pkg_path}/json")
+
+
+def get_package_metadata_or_die(package_index, package, version=None):
     """Return a dict holding the metadata for a certain package."""
-    resp = get_package_metadata(package_index, package)
+    resp = get_package_metadata(package_index, package, version)
     if resp.status != 200:
         LOG.error("pypi.org query failed: %d (%s)", resp.status, resp.reason)
         if args.verbose:
@@ -76,7 +85,7 @@ def versions(args):
 
 def show(args):
     """Implementation of the `show` subcommand."""
-    pkg = get_package_metadata_or_die(args.package_index, args.package)
+    pkg = get_package_metadata_or_die(args.package_index, args.package, args.version)
     print(json.dumps(pkg, indent=4))
 
 
@@ -91,6 +100,8 @@ if __name__ == "__main__":
     show_cmd = subparsers.add_parser("show", help="Show all PyPi metadata for a package")
     show_cmd.add_argument(
         "package", help="The package to show information for. For example, 'jinja2'.")
+    show_cmd.add_argument(
+        "version", nargs='?', default=None, help="The package version to show information for. For example, '2.9.6'.")
     show_cmd.set_defaults(action=show)
 
     versions_cmd = subparsers.add_parser("versions", help="Show available versions for a package")
