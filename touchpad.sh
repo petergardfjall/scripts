fe93e06f377c7287b921e85eb67f761630241a3a
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-11-14 20:05:40 +0100

    touchpad.sh: auto-detect device id for touchpad

--- touchpad.sh
+++ touchpad.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+TOUCHPAD_ID=${TOUCHPAD_ID}
+
 #
 # Script to turn touchpad/trackpad on/off.
 # NOTE: needs t
 #
 
 # note: may change depending on host! See: 'xinput list'
-trackpad_id=12
+if [ -z "${TOUCHPAD_ID}" ]; then
+    # do a best effort attempt to find input device id for touchpad
+    TOUCHPAD_ID=$(xinput list | grep -i touchpad | egrep --only-matching 'id=[0-9]+' | cut -d'=' -f 2)
+    if [ -z "${TOUCHPAD_ID}" ]; then
+        echo "best-effort detection of touchpad failed. Use 'xinput list' and set TOUCHPAD_ID accordingly."
+        exit 1
+    fi
+fi
 
 function die_with_error {
     echo "error: usage: $(basename ${0}) on|off"
@@ -18,10 +27,9 @@ if [ -z ${1} ]; then
 fi
 
 if [ "${1}" = "on" ]; then
-    xinput set-prop ${trackpad_id} "Device Enabled" 1
+    xinput set-prop ${TOUCHPAD_ID} "Device Enabled" 1
 elif [ "${1}" = "off" ]; then
-    xinput set-prop ${trackpad_id} "Device Enabled" 0
+    xinput set-prop ${TOUCHPAD_ID} "Device Enabled" 0
 else
     die_with_error
 fi
-
